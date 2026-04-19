60e684500d828861e6cf1e02f1046a7429990443
ShynkarenkoDamian <igoropik@gmail.com>
2026-04-19 14:41:19 +0300

    Modyfied main

--- Project_calculator/main.cpp
+++ Project_calculator/main.cpp
@@ -1,2 +1,20 @@
+#include <iostream> 
+#include "calculator.cpp"
 
+int main() {
+    Calculator calc;
+    double a, b;
 
+    cout << "First num: ";
+    cin >> a;
+
+    cout << "Second num: ";
+    cin >> b;
+
+    cout << "Addition: " <<endl;
+    cout << "Subtraction: " << calc.subtract(a, b) << endl;
+    cout << "Multiplication: " << calc.divide(a, b) << endl;
+    cout << "Division: " << calc.subtract(a, b) << endl;
+
+    return 0;
+}
