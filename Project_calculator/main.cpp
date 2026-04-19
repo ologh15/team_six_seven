1e07969b2b38b9c3ef6a437b7f644af0b756ef40
ologh1556 <ologh1556@gmail.com>
2026-04-19 13:51:11 +0300

    added main

---
+++ Project_calculator/main.cpp
@@ -0,0 +1,5 @@
+//
+// Created by gigabyte on 19.04.2026.
+//
+
+#include "main.h"
---
+++ Project_calculator/main.h
@@ -0,0 +1,14 @@
+//
+// Created by gigabyte on 19.04.2026.
+//
+
+#ifndef PROJECT_CALCULATOR_MAIN_H
+#define PROJECT_CALCULATOR_MAIN_H
+
+
+class main
+{
+};
+
+
+#endif //PROJECT_CALCULATOR_MAIN_H
