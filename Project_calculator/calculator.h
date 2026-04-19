fa897aed989cecb53d862bf003fb11be298c4650
ologh1556 <ologh1556@gmail.com>
2026-04-19 13:49:10 +0300

    create project_calculator

---
+++ Project_calculator/calculator.h
@@ -0,0 +1,13 @@
+//
+// Created by gigabyte on 19.04.2026.
+//
+
+#ifndef PROJECT_CALCULATOR_CALCULATOR_H
+#define PROJECT_CALCULATOR_CALCULATOR_H
+
+
+class calculator {
+};
+
+
+#endif //PROJECT_CALCULATOR_CALCULATOR_H
